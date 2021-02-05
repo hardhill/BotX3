86f0ec7fc53224bb0de47fa90e6b5f6bca95da85
Waclaw Bielecki <hardhill@mail.ru>
2021-02-05 16:07:04 +0800

    first commit

---
+++ BotX3/Shared/BotService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace BotX3.Shared
+{
+    public class BotService
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private BackgroundWorker worker;
+        public BotService()
+        {
+            worker = new BackgroundWorker();
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += BotStart;
+        }
+
+        public void StartService()
+        {
+            worker.RunWorkerAsync();
+        }
+
+        private void BotStart(object? sender, DoWorkEventArgs evnt)
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(16));
+            try
+            {
+                driver.Navigate().GoToUrl("https://yandex.ru");
+                IWebElement element = wait.Until(d => d.FindElement(By.Id("text")));
+                Thread.Sleep(5000);
+                driver.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
